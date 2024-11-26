52629b3395723576595de57f9ea468bf51314813
AndriKls <158137294+AndriKls@users.noreply.github.com>
2024-11-26 11:54:31 +0200

    Created product model. base html and index and details views with templates, search and pagination

---
+++ shop/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.Index.as_view(), name='index'),
+    path('product/<int:pk>', views.ProductDetail.as_view(), name='product-detail'),
+
+]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .models import Products
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
 
 # Create your views here.
+
+
+class Index(ListView):
+    model = Products
+    template_name = 'shop/index.html'
+    context_object_name = 'products'
+    paginate_by = 4
+
+
+    def get_queryset(self):
+        products = super().get_queryset()
+        item_name  = self.request.GET.get('item_name')
+        if item_name != '' and item_name is not None:
+            products = products.filter(title__icontains=item_name)
+        return products
+
+
+class ProductDetail(DetailView):
+    model = Products
+    template_name ='shop/product_detail.html'
+    context_object_name = 'product'
+
