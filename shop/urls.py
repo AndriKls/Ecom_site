86c28b9d395d01d62bd764a0a735619ede235bbd
AndriKls <158137294+AndriKls@users.noreply.github.com>
2024-11-27 23:39:28 +0200

    Final touches

--- shop/urls.py
+++ shop/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 
+
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
     path('product/<int:pk>', views.ProductDetail.as_view(), name='product-detail'),
