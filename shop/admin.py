355230125b22d83975e22d3ef24c6b28472874d5
AndriKls <158137294+AndriKls@users.noreply.github.com>
2024-11-28 00:48:23 +0200

    Admin panel changes

--- shop/admin.py
+++ shop/admin.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 from .models import Products, Order
 # Register your models here.
 
+admin.site.header = "KellaPood.ee"
+admin.site.site_title = "KellaPood.ee Admin Paneel"
+admin.site.index_title = "Tere tulemast KellaPood.ee Admin Paneeli"
 
-admin.site.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    def change_category_to_default(self, request, queryset):
+        queryset.update(category='Default')
+
+    list_display = ('title', 'price', 'discount_price','category')
+    search_fields = ('title',)
+    actions = ['change_category_to_default']
+    change_category_to_default.short_description = "Default category"
+    list_editable = ('price',)
+
+    
+admin.site.register(Products, ProductsAdmin)
 admin.site.register(Order)
